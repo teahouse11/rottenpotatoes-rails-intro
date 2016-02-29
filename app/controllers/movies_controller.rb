0ffed60dbc4049c8789e6c8129867b33d7935d54
teahouse11 <austinktang@gmail.com>
2016-02-29 19:55:12 +0000

    added link

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,11 +13,11 @@ class MoviesController < ApplicationController
   def index
     condition= params[:id]
     if condition== "title"
-      @movies=Movie.order(title)
+      @movies=Movie.order(:title)
     elsif condition == "date"
-      @movies=Movie.order(release_date)
+      @movies=Movie.order(:release_date)
     else
-      @movies = condition
+      @movies = Movie.all
     end
   end
 
